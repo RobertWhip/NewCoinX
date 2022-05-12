d15fe2b323c74e89b56d2319203a76416c40966f
RobertWhip <il.cint@mail.ru>
2022-05-12 23:49:28 +0300

    Update number.py

--- src/core/utils/number.py
+++ src/core/utils/number.py
@@ -1,6 +1,5 @@
-
 # TODO: implement BigNumber
-# TODO: write number logic (+, -, *)
+# TODO: write number logic (+, -, *, /)
 class Number:
     def __init__(self, value: str) -> None:
         self.val = value
